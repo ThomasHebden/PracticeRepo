c9bc3d0f3e311c92698766f26d708ba3d5a28cd7
ThomasHebden <hebdenthomas@gmail.com>
2015-01-05 00:54:37 +1100

    added my project euler 2

---
+++ ProjectEuler/projecteuler2.cpp
@@ -0,0 +1,22 @@
+> #include <iostream>
+using namespace std;
+int main(){
+ int a = 1;
+ int b = 2;
+ int c = 0;
+ int total = 0;
+ 
+ while (b<4000000){
+ 
+   c=a+b;
+   if (b % 2 == 0){
+             total += b;
+   a=b;
+   b=c;
+   }
+ }
+
+
+ cout << total;
+ return 0;
+}
